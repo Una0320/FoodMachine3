cf0675267eda7dd66204ec7792f05c1f33dab190
Una0320 <m55660204@gmail.com>
2023-12-19 18:37:13 +0800

    1219_1830

--- Frontend/frontend/src/Component/VideoStream.jsx
+++ Frontend/frontend/src/Component/VideoStream.jsx
@@ -4,16 +4,15 @@ import { useEffect, useState } from 'react';
 
 const VideoStream = ({ streamUrl }) => {
     return (
-        <div>
-            {/* <h2>Video Stream</h2> */}
+        // <div>
             <iframe
                 title="video-stream"
-                style={{ width: '100%', height: '420px', minWidth: 'min-content', minHeight: 'min-content' }}
+                style={{ width: '100%', height: '100%', minWidth: 'min-content', minHeight: 'min-content' }}
                 src={streamUrl}
                 frameBorder="0"
                 allowFullScreen
             ></iframe>
-        </div>
+        // </div>
     );
 };
 
