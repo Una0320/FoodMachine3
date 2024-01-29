54b8ca6b035703177bb009756e5eb3f700ba8e53
anson-liumaote <68544107+anson-liumaote@users.noreply.github.com>
2024-01-29 10:48:45 +0800

    Delete Backend/Backend directory

--- Backend/Backend/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import user
-
-class userAdmin(admin.ModelAdmin):
-	#管理頁面中，要顯示的欄位
-	list_display = ('id', 'username', 'userfeedback')
-
-admin.site.register(user, userAdmin)
-# Register your models here.
